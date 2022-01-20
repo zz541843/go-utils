4e6321aa0e4e1f83d2b5f40322b85845655508ad
zhoujun <zhoujun@bluexii.com>
2022-01-20 16:17:07 +0800

    PrintStruct

---
+++ print.go
@@ -0,0 +1,14 @@
+package jz
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+func PrintStruct(obj interface{}) {
+	b, _ := json.Marshal(obj)
+	var out bytes.Buffer
+	_ = json.Indent(&out, b, "", "    ")
+	fmt.Println(out.String())
+}
